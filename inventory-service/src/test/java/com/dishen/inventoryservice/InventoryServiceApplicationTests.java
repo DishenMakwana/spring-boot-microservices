cc27f13de5935785c17d9a0e3e2503f23580b589
DISHEN <dishenmakwana.dm@gmail.com>
2023-01-01 18:20:04 +0530

    Added: inventory microservice code and final microservices setup done

---
+++ inventory-service/src/test/java/com/dishen/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishen.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
